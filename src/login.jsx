71632a46ae5d1d4ba2fbc7e52b9bb76d6ad52d9d
Juma Qasimi <Jumaqasimi@gmail.com>
2025-02-17 10:14:54 +0100

    add login class bse compnent

---
+++ src/login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+// Login is a React compnent
+class Login extends React.Component{
+    render(){
+        return(
+            <h1>Login Page</h1>
+        );
+    }
+}
+export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import Login from './login.jsx'
+// class base component 
+class Car extends React.Component{
+ render(){
+  return <h1>hello react 2025</h1>
+ }
+
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Login />
+    <Car />
   </StrictMode>,
 )
