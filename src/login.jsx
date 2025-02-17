7db9c651023d861915cbe85417e7ab8ceebb6e79
Juma Qasimi <Jumaqasimi@gmail.com>
2025-02-17 11:34:47 +0100

    create login.css for Login.jsx

--- src/login.jsx
+++ src/login.jsx
@@ -1,13 +1,38 @@
 import React from "react";
 
-
+import './login.css';
 
 // Login is a React compnent
-class Login extends React.Component{
-    render(){
-        return(
-            <h1>Login Page</h1>
-        );
-    }
+// class Login extends React.Component{
+//     render(){
+//         return(
+//             <h1>Login Page</h1>
+//         );
+//     }
+// }
+// export default Login;
+
+import nader from "./assets/nader.jpg" 
+function Profile(){
+    return (
+        <div>
+            <img src={nader} alt="nader" width={120}/>
+        </div>
+    );
 }
-export default Login;
+
+
+export default function Galary(){
+    return(
+        <section>
+            <h1>Gallary Section</h1>
+            
+           <div className="profile">
+           <Profile />
+            <Profile />
+            <Profile />
+           </div>
+        </section>
+    );
+}
+
--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-import Login from './login.jsx'
-// class base component 
-class Car extends React.Component{
- render(){
-  return <h1>hello react 2025</h1>
- }
 
-}
+// // class base component 
+// class Car extends React.Component{
+//  render(){
+//   return <h1>hello react 2025</h1>
+//  }
+
+// }
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Login />
-    <Car />
   </StrictMode>,
 )
