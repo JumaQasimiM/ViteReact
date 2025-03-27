38a836025aeaf4cfa27437b5a3e6a22bfbccdae4
Mohammad Qasimi <qasimimohammad819@gmail.com>
2025-03-27 15:16:21 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 // import './App.css'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/login/login.jsx";
 import Dashbord from "./pages/dachbord/dachbord.jsx";
